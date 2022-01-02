d11befe92a4726d39b8a747ec5f65be3f9b68842
Mustafa <mustafasariel@gmail.com>
2022-01-02 13:59:01 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// react 'ın çalışmaya başladığı dosya
+// iki tane modül ekliyoruz.
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import React from "react";
+import myDom from "react-dom";
+
+// jSX javascript extention
+
+const arr1 = ["teknology", "books"];
+const arr2 = ["C#", "javascript", "angular"];
+const arr3 = ["chess", "javascript", "angular"];
+const arr4 = [
+  "istanbul",
+  "ankara",
+  "bartın",
+  "çanakkale",
+  "zonguldak",
+  "tokat",
+];
+
+function App1() {
+  return (
+    <div>
+      <ul>
+        {
+        arr1.map((i) => (
+          <li>
+            <h1>
+              <a href=""> {i}</a>
+            </h1>
+          </li>
+        ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+function App2() {
+  return (
+    <div>
+      {arr2.map((i) => (
+        <h1>{i}</h1>
+      ))}
+    </div>
+  );
+}
+
+function App3() {
+  return (
+    <div>
+      <select className="form-select">
+        {arr3.map((i) => (
+          <option>{i}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+function App4() {
+  return (
+    <div>
+      <ul>
+        {arr4.map((i) => (
+          <li>
+            <a href="">{i}</a>{" "}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+myDom.render(
+  <App1 />,
+
+  document.getElementById("root1")
+  //document.querySelector('#root2')
+);
+myDom.render(
+  <App2 />,
+
+  document.getElementById("root2")
+  //document.querySelector('#root2')
+);
+myDom.render(
+  <App3 />,
+
+  document.getElementById("root3")
+  //document.querySelector('#root2')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+myDom.render(
+  <App4 />,
+
+  document.getElementById("root4")
+  //document.querySelector('#root2')
+);
